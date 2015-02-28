ca55697cc2e47609bbae7705dd93dea2c650ace2
matzebond <male.schmitt@gmail.com>
2015-02-28 03:26:21 +0100

    correct install process

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
-# /bin/bash
+#!/bin/bash
+# create dirs
+mkdir -p ~/.vim/tmp/{swap,backup,undo}
+
+# create symlinks
 ln -s ~/.vim/vimrc ~/.vimrc
+
+# install Vundle
+git clone git://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install Plugins
 vim +PluginInstall +qall
